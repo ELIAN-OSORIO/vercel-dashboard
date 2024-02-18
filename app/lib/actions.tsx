74623cb9371eaa98dfd3d6abd281ef4fcf65f99b
ELIAN-OSORIO <al19760601@ite.edu.mx>
2024-02-17 22:53:20 -0800

    errores

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -5,8 +5,6 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-
-
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({ invalid_type_error: 'Please select a customer.' }),
@@ -116,7 +114,6 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
-
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
